81d914f50d542fce5ce12103aaab3ae3ccf3142b
Álvaro <alvarocabrerobarros@gmail.com>
2022-05-05 16:57:14 +0200

    Update xapiwrapper.js

--- src/xapiwrapper.js
+++ src/xapiwrapper.js
@@ -1,4 +1,4 @@
-var CryptoJS=require('../lib/cryptojs_v3.1.2.js')
+const {CryptoJS} = require('../lib/cryptojs_v3.1.2.js')
 
 // adds toISOString to date objects if not there
 // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString
